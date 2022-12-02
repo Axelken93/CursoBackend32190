368839e55ed8ea016cd72d3efb2606dc3f59e615
Flor Rubalcaba <axelken93@hotmail.com>
2022-12-02 08:47:05 -0300

    Entrega 1 - TP

--- Entrega1-TP/LogicaNegocio/login.js
+++ Entrega1-TP/LogicaNegocio/login.js
@@ -1,3 +1,17 @@
 let administrador = true
 
+function onlyAdmin(req, res, next) {
+    if (administrador) { // si es admin
+    next()
+    } else { // si no es admin, devuelvo el error
+    res.status(401).json({error:-1,descripcion:`Ruta ${req.originalUrl} metodo ${req.method} no autorizado`});
+    }
+}
+
+module.exports = {
+    onlyAdmin,
+    administrador,
+};
+
+
 module.exports= administrador
